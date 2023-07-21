5e4d704bb260706ba93557773f0d24529a9b00d2
jonfoong <jonahfoong@gmail.com>
2023-07-21 20:03:08 +0200

    optimize shiny app

--- aus_shiny/app.R
+++ aus_shiny/app.R
@@ -1,35 +1,47 @@
 library(shiny)
 library(plotly)
-library(tidyverse)
 library(shinythemes)
-library(DT)
 library(googledrive)
+library(dplyr)
 
-temp<-tempfile(fileext = '.json')
+temp <- tempfile(fileext = '.json')
+
 download.file('https://www.dropbox.com/s/twobcoxscpv77h6/rcloud-298921-bdc6e1c25160.json?dl=1',temp)
-drive_auth(path=temp)
 
-d<-list()
-n<-drive_ls()$id[1:6]
+drive_auth(path = temp)
 
-for (i in n){
+names <- c("storage", "pipeline", "longford", "lng", "gsh", "dwgm")
+
+d <- list()
+
+all_ids <- drive_ls()
+
+ind <- all_ids$name %in% names
+
+all_ids <- all_ids[ind,]
+
+for (i in 1:nrow(all_ids)){
   temp <- tempfile(fileext = '.csv')
-  d[which(n==i)]<-list(read.csv(drive_download(
-    as_id(i), path = temp, type='csv',overwrite = TRUE)$local_path, 
-    fileEncoding="latin1")[,-1])
-}
-
-for (i in 1:6){
-  assign(drive_ls()$name[i],d[i] %>% as.data.frame())
+  
+  id <- all_ids$id[i]
+  name <- all_ids$name[i]
+  
+  df <- 
+    read.csv(drive_download(
+      as_id(id), path = temp, type = 'csv', overwrite = TRUE)$local_path, 
+      fileEncoding="latin1")[,-1]
+  
+  assign(name, df)
+  
 }
 
 storage$Date <- storage$Date %>% as.Date()
 pipeline$Date <- pipeline$Date %>% as.Date()
-longford$Date<-longford$Date %>% as.Date()
-lng$Date<-lng$Date %>% as.Date()
-gsh[,4]<-gsh[,4] %>% as.Date()
-gsh[,5]<-gsh[,5] %>% as.Date()
-dwgm$gas_date<-dwgm$gas_date %>% as.Date()
+longford$Date <-longford$Date %>% as.Date()
+lng$Date <-lng$Date %>% as.Date()
+gsh[,4] <- gsh[,4] %>% as.Date()
+gsh[,5] <- gsh[,5] %>% as.Date()
+dwgm$gas_date <- dwgm$gas_date %>% as.Date()
 
 ## UI
 ui<-navbarPage(theme = shinytheme("flatly"),
@@ -85,7 +97,7 @@ server<-function(input,output){
       layout(hovermode="x unified",title = 'Longford production',
              yaxis = list(title = 'Utilisation rate (%)',
                           zeroline = TRUE)) %>%
-      config(displayModeBar = F)
+      plotly::config(displayModeBar = F)
   })
   rv2<-reactiveValues(data=storage)
   observeEvent(input$threemonthstorage,{rv2$data<-storage %>% filter(!Date<max(storage$Date)-90)})
@@ -101,7 +113,7 @@ server<-function(input,output){
       layout(hovermode="x unified",title = 'Storage',
              yaxis = list(title = 'Level (%)',
                           zeroline = TRUE)) %>%
-      config(displayModeBar = F)
+      plotly::config(displayModeBar = F)
   })
   rv3<-reactiveValues(data=pipeline)
   observeEvent(input$threemonthpipe,{rv3$data<-pipeline %>% filter(!Date<max(pipeline$Date)-90)})
@@ -117,7 +129,7 @@ server<-function(input,output){
       layout(hovermode="x unified",title = 'Pipeline flows',
              yaxis = list(title = 'Flows (TJ/d)',
                           zeroline = TRUE)) %>%
-      config(displayModeBar = F)
+      plotly::config(displayModeBar = F)
   })
   rv5<-reactiveValues(data=lng)
   observeEvent(input$threemonthlng,{rv5$data<-lng %>% filter(!Date<max(lng$Date)-90)})
@@ -133,7 +145,7 @@ server<-function(input,output){
       layout(hovermode="x unified",title = 'LNG production pipeline flows',
              yaxis = list(title = 'Flows (TJ/d)',
                           zeroline = TRUE)) %>%
-      config(displayModeBar = F)
+      plotly::config(displayModeBar = F)
   })
   rv4<-reactiveValues(data=dwgm)
   observeEvent(input$onewdwgm,{rv4$data<-dwgm %>% filter(!gas_date<max(dwgm$gas_date)-6)})
@@ -158,7 +170,7 @@ server<-function(input,output){
              yaxis = list(side = 'left', title = 'Demand', showgrid = T, zeroline = T, range=c(min(dwgm$total_gas_used,na.rm = T)-100,max(dwgm$total_gas_used,na.rm = T)+50)),
              yaxis2 = list(side = 'right', overlaying = "y", title = 'Price', showgrid = F, zeroline = T,range=c(min(dwgm$price_value,na.rm = T)-1,max(dwgm$price_value,na.rm = T)+0.2)),
              legend = list(x = 100, y = 1.2)) %>% 
-      config(displayModeBar = F)
+      plotly::config(displayModeBar = F)
   })
   output$downloadData6 <- downloadHandler(
     filename = "gsh.csv",
